710456eea115c17547333b585b30cf3388e85bc3
frustratedswede <andrew.mossefin@gmail.com>
2018-01-24 18:55:16 -0700

    initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+-Personal growth project to get the hang of Java as a language and Spring as a framework.
+
+*Currently of no real use other than for demonstration purposes*
+
+Stack: Java, Maven, Spring Boot, Bootstrap
---
+++ src/main/java/member/MemberController.java
@@ -0,0 +1,17 @@
+package member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MemberController {
+
+  @RequestMapping("/member")
+  public String member(@RequestParam(value="name", required=false,
+  defaultValue="you aren't currently logged in") String name, Model model) {
+    model.addAttribute("name", name);
+    return "member";
+  }
+}
