6ef3ec3087a0dc8201d1f4278ee3be03f2095ab0
frustratedswede <andrew.mossefin@gmail.com>
2018-01-24 23:45:51 -0700

    edit folder namespace

--- src/main/java/com/amossefin/Application.java
+++ src/main/java/com/amossefin/Application.java
@@ -1,4 +1,4 @@
-package lib;
+package com.amossefin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/amossefin/MemberController.java
+++ src/main/java/com/amossefin/MemberController.java
@@ -1,4 +1,4 @@
-package lib;
+package com.amossefin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
